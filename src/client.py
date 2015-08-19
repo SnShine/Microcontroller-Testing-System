69e6850d7800978bfab91c2f7f32fc516d6b9768
Surya Cheedella <7132995+SnShine@users.noreply.github.com>
2015-08-19 16:58:08 +0530

    modified server and client files

--- src/client.py
+++ src/client.py
@@ -1,33 +1,52 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start_client():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('172.16.48.241', 8680)
-print ("Connecting to", server_address) 
-sock.connect(server_address)
-print ("Sucessfully connected to server...")
+    # Connect the socket to the port where the server is listening
+    server_address = ('172.16.73.218', 8604)
+    print "Connecting to", server_address, "..."
 
-while True:
-    print ("\nEnter your command: ",)
-    user_command= raw_input()
-    print(user_command)
     try:
-        # Send data
-        message = user_command
-        #print ("Sending command to server...")
-        sock.sendall(message)
-        print("ppp")
-        if message== "close all":
-            print("Closing connection and stopping server!")
-            sock.close()
-            break
-        # receive data
-        data= sock.recv(64)
-        print ("Response: ", data)
+        sock.connect(server_address)
+        print "Sucessfully connected to server!"
     except:
-        print("Error has occured! (client)")
+        print "Unable to connect to the server!"
+        sys.exit()
+
+    return sock
+
+def talk_to_server(sock):
+    print "\nEnter your command:",
+    user_command= raw_input()
+    #print(user_command)
+    try:
+        # Send command
+        message = user_command
+        sock.sendall(message)
+
+        if message== "close all":
+            print "Closing connection and stopping server!"
+            sock.close()
+            return 0
+        
+        # receive output
+        data= sock.recv(64)
+        print "Response: ", data
+    except:
+        print "Error has occured! (client)"
         print(sys.stderr)
-        pass
+        sock.close()
+        sys.exit()
+
+
+if __name__== "__main__":
+    sock= start_client()
+
+    while True:
+        ret= talk_to_server(sock)
+        if ret== 0:
+            break
+        
